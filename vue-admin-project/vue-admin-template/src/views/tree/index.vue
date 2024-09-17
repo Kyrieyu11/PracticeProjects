9f932a868a118355050ea39b9f3934bab2ba32b5
kyrieyu <kyrieyu111@gmail.com>
2024-09-17 20:12:46 +0800

    test

---
+++ vue-admin-project/vue-admin-template/src/views/tree/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="app-container">
+    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
+
+    <el-tree
+      ref="tree2"
+      :data="data2"
+      :props="defaultProps"
+      :filter-node-method="filterNode"
+      class="filter-tree"
+      default-expand-all
+    />
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      filterText: '',
+      data2: [{
+        id: 1,
+        label: '贷款组',
+        children: [{
+          id: 4,
+          label: 'Level two 1-1'
+        }]
+      }, {
+        id: 2,
+        label: '业务组',
+        children: [{
+          id: 5,
+          label: 'Level two 2-1'
+        }]
+      }, {
+        id: 3,
+        label: '办公组',
+        children: [{
+          id: 7,
+          label: 'Level two 3-1'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val)
+    }
+  },
+
+  methods: {
+    filterNode(value, data) {
+      if (!value) return true
+      return data.label.indexOf(value) !== -1
+    }
+  }
+}
+</script>
+
